cf0127308dc69227de2c4c7128c1d41143784c27
Sh1iba <zafar.ibragimov@proton.me>
2026-07-11 22:15:19 +0500

    feat: create first servlet

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    war
 }
 
 group = "io.github.sh1iba"
@@ -13,8 +14,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.war {
+    archiveFileName = "ROOT.war"
 }
---
+++ src/main/java/io/github/sh1iba/MyFirstServlet.java
@@ -0,0 +1,18 @@
+package io.github.sh1iba;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class MyFirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello my first servlet");
+    }
+}
