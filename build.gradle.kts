f4805ec88a1f87be46cdc0699f2d28f347f65a18
Sh1iba <zafar.ibragimov@proton.me>
2026-07-13 23:31:42 +0500

    feat: add two tables Currencies and ExchangeRates

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
+    implementation("org.xerial:sqlite-jdbc:3.53.2.0")
 }
 
 tasks.test {
--- src/main/java/io/github/sh1iba/MyFirstServlet.java
+++ src/main/java/io/github/sh1iba/MyFirstServlet.java
@@ -7,12 +7,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+//application/json
+//text/html
+//text/plain
+//application/xml
 
 @WebServlet("/hello")
 public class MyFirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello my first servlet");
+        resp.setContentType("text/plain");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("I'm Batman");
     }
 }
